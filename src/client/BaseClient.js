e139efb09ecbb4a64ce30a82f796de0e09bd6277
Fifixex <122852085+Fifixex@users.noreply.github.com>
2024-12-20 21:37:01 +0000

    :sparkles: feat: add `_attachEvents` & `_handlePacket` private methods

--- src/client/BaseClient.js
+++ src/client/BaseClient.js
@@ -13,4 +13,26 @@ export class BaseClient extends EventEmitter {
 	destroy() {
 		this.removeAllListeners()
 	}
+
+	/**
+	 * Increments max listeners by one, if they are not zero.
+	 * @private
+	 */
+	incrementMaxListeners() {
+		const maxListeners = this.getMaxListeners()
+		if (maxListeners !== 0) {
+			this.setMaxListeners(maxListeners + 1)
+		}
+	}
+
+	/**
+	 * Decrements max listeners by one, if they are not zero.
+	 * @private
+	 */
+	decrementMaxListeners() {
+		const maxListeners = this.getMaxListeners()
+		if (maxListeners !== 0) {
+			this.setMaxListeners(maxListeners - 1)
+		}
+	}
 }
--- src/client/Client.js
+++ src/client/Client.js
@@ -1,26 +1,9 @@
 //@ts-check
 
-import { BASE_URL, GATEWAY_URL } from '../config.js'
 import { BaseClient } from './BaseClient.js'
-import WebSocket from 'ws'
-
-class Message {
-	constructor(client, data) {
-		this.client = client
-		this.message = data
-	}
-
-	async reply(content) {
-		await this.client.sendMessage(this.message.channel_id, { ...content })
-	}
-
-	async delete() {
-		await this.client.deleteMessage(this.message.channel_id, this.message.id)
-	}
-	get content() {
-		return this.message.content
-	}
-}
+import { ActionsManager } from './actions/ActionsManager.js'
+import { WebSocketManager } from '../ws/WebSocketManager.js'
+import PacketHandlers from './handlers/index.js'
 
 /**
  * The main hub for interacting with the Discord API.
@@ -33,82 +16,67 @@ export class Client extends BaseClient {
 		super(options)
 		if (options.intents === undefined) throw Error('Intents undefined!')
 
-		this.intents = options.intents
-		this.token = options.token || process.env.DISCORD_TOKEN
+		this.actions = new ActionsManager(this)
 
-		if (!this.token) throw new Error('No token provided')
+		const wsOptions = {
+			...options.ws,
+			intents: options.intents,
+			token: null,
+		}
+
+		this.ws = new WebSocketManager(wsOptions)
+		this.intents = options.intents
+
+		Reflect.defineProperty(this, 'token', { writable: true })
+
+		if (!this.token && 'DISCORD_TOKEN' in process.env)
+			this.token = process.env.DISCORD_TOKEN
+		else if (options.ws.token) this.token = options.ws.token
+		else this.token = null
+
+		this._attachEvents()
 	}
 
 	async login(token = this.token) {
 		if (!token || typeof token !== 'string') throw 'Token invalid!'
 
 		this.token = token.replace(/^(Bot|Bearer)\s*/i, '')
-		console.log(`Logging in with token: ${this._censoredToken}`)
+		console.debug(`Logging in with token: ${this._censoredToken}`)
 
-		await this.connect()
-		return this.token
-	}
+		this.ws.setToken(this.token)
 
-	async connect() {
-		this.ws = new WebSocket(GATEWAY_URL)
-		this.ws.on('open', () => this.identify())
-		this.ws.on('close', () => this.connect())
-		this.ws.on('error', () => this.ws?.close())
-
-		this.ws.on('message', (data) => {
-			const message = JSON.parse(data.toString())
-			if (message.op === 0 && message.t === 'MESSAGE_CREATE') {
-				this.emit('message', new Message(this, message.d))
-			}
-		})
-	}
-
-	identify() {
-		this.ws?.send(
-			JSON.stringify({
-				op: 2,
-				d: {
-					token: this.token,
-					intents: this.intents,
-					properties: {
-						$os: 'linux',
-						$browser: 'my-library',
-						$device: 'my-library',
-					},
-				},
-			}),
-		)
-	}
-
-	async destroy() {
-		if (this.ws) {
-			this.ws.close()
+		try {
+			await this.ws.connect()
+			return this.token
+		} catch (error) {
+			this.destroy()
+			throw error
 		}
+	}
+
+	destroy() {
+		super.destroy()
+		this.ws.destroy()
 		this.token = null
 	}
 
-	async sendMessage(channelId, payload) {
-		const response = await fetch(`${BASE_URL}/channels/${channelId}/messages`, {
-			method: 'POST',
-			headers: {
-				Authorization: this.token,
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(payload),
-		})
-
-		return response.json()
+	_handlePacket(packet, shardId) {
+		if (PacketHandlers[packet.t]) {
+			PacketHandlers[packet.t](this, packet, shardId)
+		}
 	}
 
-	async deleteMessage(channelId, messageId) {
-		await fetch(`${BASE_URL}/channels/${channelId}/messages/${messageId}`, {
-			method: 'DELETE',
-			headers: {
-				Authorization: this.token,
-				'Content-Type': 'application/json',
-			},
-		})
+	_attachEvents() {
+		this.ws.on('debug', (message, shardId) =>
+			this.emit(
+				'debug',
+				`[WS => ${typeof shardId === 'number' ? `Shard ${shardId}` : 'Manager'}] ${message}`,
+			),
+		)
+		this.ws.on('dispatch', this._handlePacket.bind(this))
+		this.ws.on('ready', (data) => console.log(data))
 	}
+
 	/**
 	 * Partially censored client token for debug logging purposes.
 	 * @type {?string}
